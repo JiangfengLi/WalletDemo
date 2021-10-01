1e05c257fdfa14ae3ce4b69db460ab8f1cb38b5b
Zxu49 <771643358@qq.com>
2021-10-01 12:02:35 +0800

    Implement QR scanner into text
    Implement QR scanner into text

--- app/src/main/java/com/edu/walletdemo/MainActivity.kt
+++ app/src/main/java/com/edu/walletdemo/MainActivity.kt
@@ -1,27 +1,35 @@
 package com.edu.walletdemo
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import io.github.g00fy2.quickie.QRResult
+import io.github.g00fy2.quickie.ScanQRCode
 
 class MainActivity : AppCompatActivity() {
     private var btn: Button? = null
+    private var tvresult: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scanQrCode = registerForActivityResult(ScanQRCode(), ::handleResult)
 
         tvresult = findViewById<View>(R.id.tvresult) as TextView
         btn = findViewById<View>(R.id.btn) as Button
         btn!!.setOnClickListener {
-            val intent = Intent(this@MainActivity, ScanActivity::class.java)
-            startActivity(intent)
+            scanQrCode.launch(null)
         }
     }
 
-    companion object {
-        var tvresult: TextView? = null
+    fun handleResult(result: QRResult) {
+        val text = when (result) {
+            is QRResult.QRSuccess -> result.content.rawValue
+            QRResult.QRUserCanceled -> "User canceled"
+            QRResult.QRMissingPermission -> "Missing permission"
+            is QRResult.QRError -> "${result.exception.javaClass.simpleName}: ${result.exception.localizedMessage}"
+        }
+        tvresult!!.text = text
     }
 }
