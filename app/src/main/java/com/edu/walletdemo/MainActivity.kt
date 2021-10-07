acbc2ddbff63a8a4cc2f77b638733462b051eb43
Zxu49 <771643358@qq.com>
2021-10-07 23:52:47 +0800

    import SDK

--- app/src/main/java/com/edu/walletdemo/MainActivity.kt
+++ app/src/main/java/com/edu/walletdemo/MainActivity.kt
@@ -7,12 +7,26 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.coinbase.wallet.core.*
+import com.coinbase.walletlink.WalletLink
+import com.coinbase.walletlink.models.ClientMetadataKey
+import com.coinbase.walletlink.models.HostRequestId
 import io.github.g00fy2.quickie.QRResult
 import io.github.g00fy2.quickie.ScanQRCode
+import java.net.URL
+import java.util.*
+import io.reactivex.*
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
 
 class MainActivity : AppCompatActivity() {
     private var btn: Button? = null
     private var tvresult: TextView? = null
+    private var walletLink : WalletLink? = null
+    private val disposeBag = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,12 +47,79 @@ class MainActivity : AppCompatActivity() {
             is QRResult.QRError -> "${result.exception.javaClass.simpleName}: ${result.exception.localizedMessage}"
         }
         tvresult!!.text = text
-        redirection(text)
+        initWalletLink(text)
     }
 
-    private fun redirection (text : String) {
+    private fun redirection(text: String) {
         val openURL = Intent(Intent.ACTION_VIEW)
         openURL.data = Uri.parse(text)
         startActivity(openURL)
     }
+
+    // To pair the device with a browser after scanning WalletLink QR code
+    private fun initWalletLink(text: String) {
+        walletLink = WalletLink(notificationUrl = URL(text), applicationContext)
+        val sessionId = ""
+        val secret = ""
+        val serverUrl = URL(text)
+        val userId = ""
+        val primaryAddress = ""
+        val version =""
+        walletLink!!.link(
+            sessionId = sessionId,
+            secret = secret,
+            url = serverUrl,
+            userId = userId,
+            version = version,
+            metadata = mapOf(ClientMetadataKey.EthereumAddress to primaryAddress)
+        ).subscribeBy(onSuccess = {
+            print("New WalletLink connection was established")
+        }, onError = { error ->
+            print("Error while connecting to WalletLink server (walletlink)")
+        }).addTo(disposeBag)
+    }
+
+//    // Listen on incoming requests
+//    private fun listen () {
+//        // Listen on incoming requests
+//        walletLink.requests
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribeBy(onNext = { request ->
+//                // New unseen request
+//            })
+//            .addTo(disposeBag)
+//    }
+//
+//    // Approve DApp permission request (EIP-1102)
+//    private fun approveRequest (hostRequestId : HostRequestId) {
+//        walletLink.approveDappPermission(hostRequestId)
+//        .subscribeBy(onSuccess = {
+//            // Browser received EIP-1102 approval
+//        }, onError = { error ->
+//            // Browser failed to receive EIP-1102 approval
+//        }).addTo(disposeBag)
+//    }
+
+    // Approve a given transaction/message signing request
+    private fun approveRequest (hostRequestId : HostRequestId, signedData : ByteArray) {
+        walletLink!!.approve(hostRequestId, signedData)
+            .subscribeBy(onSuccess = {
+                print("Received request approval")
+            }, onError = { error ->
+                print("Failed to receive request approval")
+            })
+            .addTo(disposeBag)
+    }
+
+    // Reject transaction/message/EIP-1102 request
+    private fun rejectRequest (hostRequestId : HostRequestId, signedData : ByteArray) {
+        walletLink!!.approve(hostRequestId, signedData)
+            .subscribeBy(onSuccess = {
+                print("Received request approval")
+            }, onError = { error ->
+                print("Failed to receive request approval")
+            })
+            .addTo(disposeBag)
+    }
+
 }
